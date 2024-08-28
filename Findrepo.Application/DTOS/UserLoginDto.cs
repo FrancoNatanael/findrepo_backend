4500103b57405007b649bf42e62a145a80451264
Franco Natanael Tomaino <91550023+FrancoNatanael@users.noreply.github.com>
2024-08-28 17:46:47 -0300

    Delete Findrepo.Application directory

--- Findrepo.Application/DTOS/UserLoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Findrepo.Application.DTOS
-{
-    public class UserLoginDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-    }
-}
